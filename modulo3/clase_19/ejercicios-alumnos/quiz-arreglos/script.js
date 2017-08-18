900554956ea62f8ad49cc80a36c8f1725fbc8a0b
Raymi Saldomando <raymi@Raymis-MacBook-Pro.local>
2017-08-18 18:09:25 -0500

    clase_20

--- modulo3/clase_19/ejercicios-alumnos/quiz-arreglos/script.js
+++ modulo3/clase_19/ejercicios-alumnos/quiz-arreglos/script.js
@@ -6,7 +6,7 @@
   Utiliza arreglos para hacer este ejercicio. Pista: los arreglos que crees tienen que ver con las preguntas y sus respectivas respuestas.
 */
 
-
+/*
 var respuestasCorrectas = 0;
 
  var respuesta1 = prompt('pregunta 1');
@@ -28,6 +28,49 @@ if(respuesta4 === 'bloh') {
   respuestasCorrectas = respuestasCorrectas + 1;
 }
 if(respuesta5 === 'bluh') {
+  respuestasCorrectas = respuestasCorrectas + 1;
+}
+
+if(respuestasCorrectas === 5){
+  document.write('Has ganado la corona de oro!');
+}else if(respuestasCorrectas === 3) {
+  document.write('Has ganado la corona de plata');
+}else if(respuestasCorrectas < 3){
+  document.write('Perdiste :(');
+}
+
+*/
+
+var respuestasCorrectas = 0
+
+var qa = [
+  ['Pregunta1', 'Respuesta 1'],
+  ['Pregunta2', 'Respuesta 2'],
+  ['Pregunta3', 'Respuesta 3'],
+  ['Pregunta4', 'Respuesta 4'],
+  ['Pregunta5', 'Respuesta 5']
+]
+
+var pregunta1 = prompt(qa[0][0]);
+var pregunta2 = prompt(qa[1][0]);
+var pregunta3 = prompt(qa[2][0]);
+var pregunta4 = prompt(qa[3][0]);
+var pregunta5 = prompt(qa[4][0]);
+
+
+if(pregunta1 === qa[0][1]) {
+  respuestasCorrectas = respuestasCorrectas + 1;
+}
+if(pregunta2 === qa[1][1]) {
+  respuestasCorrectas = respuestasCorrectas + 1;
+}
+if(pregunta3 === qa[2][1]) {
+  respuestasCorrectas = respuestasCorrectas + 1;
+}
+if(pregunta4 === qa[3][1]) {
+  respuestasCorrectas = respuestasCorrectas + 1;
+}
+if(pregunta5 === qa[4][1]) {
   respuestasCorrectas = respuestasCorrectas + 1;
 }
 
---
+++ modulo3/clase_20/ejercicios-clase/Funciones-parametros-argumentos/script.js
@@ -0,0 +1,54 @@
+/*
+
+Para que el código de nuestras funciones pueda ser rehusable, utilizamos lo que se conoce como argumentos
+
+
+
+function suma(num1, num2) {
+  return num1 + num2;
+}
+
+console.log(suma(3, 5));
+console.log(suma(13, 7));
+console.log(suma(3, 25));
+
+
+*/
+
+function generarNumeroAleatorio(numero) {
+  var numeroAleatorio = Math.floor( Math.random() * numero ) + 1;
+  document.write('<div class="container"><p>' + numeroAleatorio + '</p></div>');
+}
+
+generarNumeroAleatorio(40);
+
+/*
+
+Ejemplo con el ejercicio del Cuenta Cuentos
+
+var sustantivo = prompt('Escribe un sustantivo' + preguntasRestantes);      
+var adjetivo = prompt('Escribe un adjetivo' + preguntasRestantes);
+var verbo = prompt('Escribe un verbo' + preguntasRestantes);     
+alert("¿Estás listo para tu historia?");      
+var historia = '<p> Habia una vez un ' + sustantivo + ' ' + 'que era muy ' + adjetivo + ' ' + 'y le gustaba salir a ' + verbo + '</p>'      
+document.write(historia);
+
+*/
+
+
+/*
+var sustantivo = mensajes('Escribe un sustantivo');
+var adjetivo = mensajes('Escribe un adjetivo');
+var verbo = mensajes('Escribe un verbo');
+
+function mensajes(palabras){
+  var palabra = prompt(palabras);
+  return palabra;
+}
+
+function oracion(){
+  document.write('<p> Habia una vez un ' + sustantivo + ' ' + 'que era muy ' + adjetivo + ' ' + 'y le gustaba salir a ' + verbo + '</p>');
+}
+
+oracion();
+*/
