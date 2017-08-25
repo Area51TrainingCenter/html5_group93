9fd057021be12006780fb9e8305f5b9acde78fbf
Raymi Saldomando <raymi15@gmail.com>
2017-08-25 18:23:59 -0500

    clase_23

--- modulo3/clase_22/ejercicios-alumnos/todo-app/main.js
+++ modulo3/clase_22/ejercicios-alumnos/todo-app/main.js
@@ -9,3 +9,15 @@
 	7. Clickear dicho elemento
 	8. Obtener el valor del elemento y cambiar el titulo por defecto por dicho valor
 */
+
+/*Lista de pasos para hacer todo-app
+
+	1. Obtener el campo de texto
+	2. Obtener el boton
+	3. Activar el evento click
+	4. Una vez que el botón es clickeado, obtner el valor del campo de texto
+	5. Poner el valor debajo de mi app
+	6. Obtener el elemento de la lista que voy a clickear
+	7. Clickear dicho elemento
+	8. Obtener el valor del elemento y cambiar el titulo por defecto por dicho valor
+*/
